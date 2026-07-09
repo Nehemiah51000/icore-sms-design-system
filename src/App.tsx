7a3484c625574b763c0bbb66d07f5c27be03e04c
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2026-07-09 13:44:01 +0300

    FEAT: Replace App component layout with routing setup for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+
 function App() {
   return (
-    <div className='min-h-screen bg-muted-100 p-8'>
-      <header className='mb-8 flex items-center gap-3'>
-        <img
-          src='/ic_frame_2.svg'
-          alt='ICORE'
-          className='h-10 w-10 rounded-lg'
-        />
-        <h1 className='text-xl font-semibold text-ink-700'>
-          ICORE SMS Design System
-        </h1>
-      </header>
-
-      <section>
-        <img
-          src='/ICORE_logo_last_iteration.svg'
-          alt='ICORE Information Systems Ltd'
-          className='h-16'
-        />
-      </section>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      {/* Component detail routes get added here as we build each one */}
+    </Routes>
   );
 }
 
