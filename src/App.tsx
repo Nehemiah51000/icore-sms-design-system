3f9ece03671876ba2f63ea7f52fc6d2e4dec4b9c
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2026-07-09 14:24:57 +0300

    FEAT: Add routes for Colors and Typography pages in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { ColorsPage } from './pages/ColorPage';
+import { TypographyPage } from './pages/TypographyPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/colors' element={<ColorsPage />} />
+      <Route path='/typography' element={<TypographyPage />} />
       {/* Component detail routes get added here as we build each one */}
     </Routes>
   );
